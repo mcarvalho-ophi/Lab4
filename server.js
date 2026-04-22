770c4b6da8c45848da0b2321648951aadde251db
Marcela Carvalho <mcarv6@icloud.com>
2026-04-22 19:19:07 +0100

    alteraçoes feitas para webapp e nodejs com servidorweb express js

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+// Serve static files from /src (css, images, etc.)
+app.use(express.static(path.join(__dirname, "src")));
+
+// Home route
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "src", "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
